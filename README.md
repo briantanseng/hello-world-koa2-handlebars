1563b4691c2bd5a9c068f0bcdc3bec53fce0bf79
Brian Tan Seng <brian.tanseng@98labs.com>
2018-09-02 21:43:03 +0800

    added application files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # hello-world-koa2-handlebars
 Hello world example using Koa 2 with handlebars templates
+
+## Clone this repository
+```
+git clone https://github.com/briantanseng/hello-world-koa2-handlebars.git
+cd hello-world-koa2-handlebars
+```
+
+## Download dependencies
+```
+npm install
+```
+npm start
+```
+
---
+++ app.js
@@ -0,0 +1,25 @@
+const path   = require('path');
+const views  = require('koa-views');
+const Koa    = require('koa');
+const Router = require('koa-router');
+
+const app    = new Koa();
+const router = new Router();
+
+app.use(views(path.join(__dirname + '/'), {
+  map: {
+    html: 'handlebars'
+  }
+}));
+
+
+router.get('/', async function(ctx, next) {
+    const data = { version: 1, message: "Hello World!", time: new Date() };
+    console.log('router.get /', data);
+    await ctx.render('index', { data });
+});
+
+app.use(router.routes());
+
+if (!module.parent) app.listen(3000);
+console.log('Server running on 3000');
